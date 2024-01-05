70a26b5aa8adc3b3cab0a31b110961d473bebdcf
air-one1 <erwann.eboule@etu.imt-nord-europe.fr>
2024-01-05 12:04:17 +0100

    Android project

--- app/src/main/java/com/example/proyecto/MainActivity.java
+++ app/src/main/java/com/example/proyecto/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.proyecto;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.view.View;
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void registrar(View view) {
+            // Code à exécuter lors du clic sur le TextView
+            Intent intent = new Intent(this, registrarse_actividad.class);
+            startActivity(intent);
+        }
     }
 
 }
