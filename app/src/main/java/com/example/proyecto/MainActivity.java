bbbbc2020459020f5ad4e802ec2267fa0e4e10d4
andreadiiez <andrea.diez01@gmail.com>
2024-01-05 14:48:42 +0100

    update registrarse

--- app/src/main/java/com/example/proyecto/MainActivity.java
+++ app/src/main/java/com/example/proyecto/MainActivity.java
@@ -17,6 +17,5 @@ public class MainActivity extends AppCompatActivity {
             Intent intent = new Intent(this, registrarse_actividad.class);
             startActivity(intent);
         }
-    }
 
 }
