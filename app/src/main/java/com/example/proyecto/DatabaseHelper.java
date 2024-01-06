05e03e9a123ae843e5d9433f8e48368d4ddb9e0c
air-one1 <erwann.eboule@etu.imt-nord-europe.fr>
2024-01-06 04:52:04 +0100

    Update
    LOGIN & REGISTRARSE OK

---
+++ app/src/main/java/com/example/proyecto/DatabaseHelper.java
@@ -0,0 +1,55 @@
+package com.example.proyecto;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "nom_de_votre_base_de_donnees";
+    private static final int DATABASE_VERSION = 1;
+
+    // Définissez le nom de votre table et les noms de colonnes
+    public static final String TABLE_NAME = "utilisateurs";
+    public static final String COLUMN_ID = "_id";
+    public static final String COLUMN_CORREO = "correo";
+    public static final String COLUMN_NOMBRE = "nombre";
+    public static final String COLUMN_PASSWORD = "password";
+
+    // Requête SQL de création de la table
+    private static final String TABLE_CREATE =
+            "CREATE TABLE " + TABLE_NAME + " (" +
+                    COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    COLUMN_CORREO + " TEXT, " +
+                    COLUMN_NOMBRE + " TEXT, " +
+                    COLUMN_PASSWORD + " TEXT);";
+
+    public DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(TABLE_CREATE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public boolean verifierUtilisateur(String email, String motDePasse) {
+        String[] columns = {COLUMN_ID};
+        SQLiteDatabase db = this.getReadableDatabase();
+        String selection = COLUMN_CORREO + " = ?" + " AND " + COLUMN_PASSWORD + " = ?";
+        String[] selectionArgs = {email, motDePasse};
+        Cursor cursor = db.query(TABLE_NAME, columns, selection, selectionArgs, null, null, null);
+        int count = cursor.getCount();
+        cursor.close();
+        db.close();
+
+        return count > 0;
+    }
+}
